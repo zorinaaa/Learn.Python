405e07367bdb03f7b086497631675c851aa2682a
Eugenia Zorina <evgeniya247@icloud.com>
2017-03-11 18:56:53 +0300

    Added bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+def greet_user(bot, update):
+    print('Вызван /start')
+    print(update)
+    bot.sendMessage(update.message.chat_id, text = 'Пообщаемся?')
+
+def show_error(bot, update, error):
+    print(error)
+
+def talk_to_me(bot, update):
+    print(update.message.text)
+    bot.sendMessage(update.message.chat_id, update.message.text)
+
+def main():
+    updater = Updater('300303088:AAGtkjxU_MHJKLzhEdrlgPI33Ky8ifRuvZQ')
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+
+    dp.add_error_handler(show_error)
+    updater.start_polling()
+    updater.idle()
+main()
--- info.py
+++ info.py
@@ -3,4 +3,4 @@ first_name = input ('Vvedite imya: ')
 user_info['first_name'] = 'first_name'
 last_name = input ('Vvedite familiyu: ')
 user_info['last_name'] = 'last_name'
-print user_info
+print (user_info)
